47a0877ad88f1597ff8a3dfa4414655043110ce0
Ben Silverman <ben@performantsoftware.com>
2022-11-01 16:41:48 -0400

    Adjust nav and search for smaller displays

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,19 @@ import {
     EuiHeaderLinks,
     EuiHeaderLink,
 } from "@elastic/eui";
+import { appendIconComponentCache } from "@elastic/eui/es/components/icon/icon";
+import { icon as EuiIconArrowEnd } from "@elastic/eui/es/components/icon/assets/arrowEnd";
+import { icon as EuiIconArrowStart } from "@elastic/eui/es/components/icon/assets/arrowStart";
+import { icon as EuiIconApps } from "@elastic/eui/es/components/icon/assets/apps";
 import { NavLink } from "react-router-dom";
+
+// icon component cache required for dynamically imported EUI icons in Vite;
+// see https://github.com/elastic/eui/issues/5463
+appendIconComponentCache({
+    arrowEnd: EuiIconArrowEnd,
+    arrowStart: EuiIconArrowStart,
+    apps: EuiIconApps,
+});
 
 /**
  * Site navigation functional component.
@@ -19,7 +31,7 @@ function Navigation() {
             <EuiHeader>
                 <EuiHeaderSection>
                     <EuiHeaderSectionItem border="none">
-                        <EuiHeaderLinks>
+                        <EuiHeaderLinks popoverBreakpoints="none">
                             <NavLink to="/" end>
                                 <EuiHeaderLink>
                                     Samuel Beckett Letters
