4128ea194a7c99382c4de165d39d278ab169522a
Jay Varner <jayvarner@gmail.com>
2023-09-15 12:15:27 -0400

    Add tracking

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 
@@ -9,11 +9,29 @@ import Footer from "./components/Footer";
  * @returns {React.Component} React component.
  */
 export function Root() {
+    const location = useLocation();
+    const trackerRef = useRef();
+
+    useEffect(() => {
+        window?.ga("send", "pageview");
+        trackerRef.current?.setAttribute(
+            "src",
+            "https://matomo.ecdsdev.org/matomo.php?idsite=59&rec=1",
+        );
+    }, [location]);
+
     return (
         <>
             <Navigation />
             <Outlet />
             <Footer />
+            <img
+                ref={trackerRef}
+                referrerPolicy="no-referrer-when-downgrade"
+                src="https://matomo.ecdsdev.org/matomo.php?idsite=59&amp;rec=1"
+                alt=""
+                style={{ border: 0 }}
+            />
         </>
     );
 }
