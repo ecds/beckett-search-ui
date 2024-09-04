35b75dd1fbfdbddde1ec3ff98be7ea6e11643139
Jay Varner <jayvarner@gmail.com>
2024-09-04 18:49:35 -0400

    Form rework

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,43 @@
+import { useRef } from "react";
+
 export function Form() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const messageRef = useRef();
+    const sendMail = async () => {
+        await fetch("/mail.php", {
+            method: "POST",
+            body: `name=${encodeURI(nameRef.current.value)}&email=${encodeURI(
+                emailRef.current.value,
+            )}&message=${encodeURI(messageRef.current.value)}`,
+            headers: {
+                "content-type": "application/x-www-form-urlencoded",
+            },
+        });
+    };
+
+    const handleSubmit = () => {
+        sendMail();
+        return false;
+    };
     return (
-        <form action="/mail.php" method="post">
-            <label for="name">First Name:</label>
-            <input type="text" id="name" name="name" required />
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">First Name:</label>
+            <input ref={nameRef} type="text" id="name" name="name" required />
             <br />
 
-            <label for="email">Email Address:</label>
-            <input type="email" id="email" name="email" required />
+            <label htmlFor="email">Email Address:</label>
+            <input
+                ref={emailRef}
+                type="email"
+                id="email"
+                name="email"
+                required
+            />
             <br />
 
-            <label for="message">Message:</label>
-            <textarea id="message" name="message" required></textarea>
+            <label htmlFor="message">Message:</label>
+            <textarea ref={messageRef} id="message" name="message" required />
             <br />
 
             <input type="submit" value="Send"></input>
